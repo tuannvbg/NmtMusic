95b84f9a28bb48844bc6f2844b053bfa424092c6
Your Nameter <thanhnguyenvip09@gmail.com>
2021-11-27 10:22:15 +0700

    click here

--- zingmp3.js
+++ zingmp3.js
@@ -206,7 +206,7 @@ var title = document.querySelector('#title');
 var cover = document.querySelector('#cover');
 
 //title
-var songs = ['LastDance', 'Fit', 'HaruHaru', 'Blue', 'IfYou', 'LetNotFallInLove', 'Loser'];
+var songs = ['lastdance', 'Fit', 'HaruHaru', 'Blue', 'IfYou', 'LetNotFallInLove', 'Loser'];
 let songIndex = 0;
 
 loadsong(songs[songIndex]);
